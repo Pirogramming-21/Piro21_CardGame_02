b8298918c46f5924aaa1fbe500788b8fedd4e3e7
devkev00 <kimminsoo116@gmail.com>
2024-07-19 00:03:29 +0900

    [Feat] resolving conflict

--- apps/game/apps.py
+++ apps/game/apps.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 from django.apps import AppConfig
 
 
 class GameConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.game'
-=======
-from django.apps import AppConfig
-
-
-class GameConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.game'
->>>>>>> develop
