754bff55e8b997619957a4cf3715350650552da1
shwn <zxz8673@gmail.com>
2024-07-19 01:51:05 +0900

    [feat] 비밀번호 규칙 간소화, 패스워드찾기 템플릿 적용

--- config/settings.py
+++ config/settings.py
@@ -92,21 +92,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
+# 비밀번호 검증이 너무 빡세서 수정함.
+AUTH_PASSWORD_VALIDATORS = [ ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
