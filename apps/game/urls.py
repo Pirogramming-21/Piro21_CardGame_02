12f570bdb199ad4d745af91c8b9f430a83cf720d
shwn <zxz8673@gmail.com>
2024-07-19 01:01:59 +0900

    [add] feature/SH (1) 채우기(OAuth 제외)

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -5,4 +5,9 @@ app_name = 'game'
 
 urlpatterns = [
     path('', main, name='main'),
+    path('ranking/', ranking, name='ranking'),
+    path('attack/', attack, name='attack'),
+    path('counter/', counter, name='counter'),
+    path('game/list/', game_list, name='game_list'),
+    path('game/<int:pk>/', game_detail, name='game_detail'),
 ]
--- apps/game/views.py
+++ apps/game/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
 def main(req):
     return render(req, 'main.html')
+
+def attack(req):
+    return render(req, 'attack.html')
+
+def counter(req):
+    return render(req, 'counter.html') 
+
+def game_list(req):
+    return render(req, 'list.html')
+
+def game_detail(req, pk):
+    return render(req, 'detail.html', {'pk': pk})
+
+def ranking(req):
+    return render(req, 'ranking.html')
