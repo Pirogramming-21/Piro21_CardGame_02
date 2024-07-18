5158de7bb07480e88bea29cc1853ca9cea9df11c
devkev00 <kimminsoo116@gmail.com>
2024-07-18 18:51:05 +0900

    [Feat] list.html 추가, game.urls.py, game.views.py 추가

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from . import views
 from .views import *
 
 app_name = 'game'
 
 urlpatterns = [
-    path('', main, name='main'),
+    path('game-history/', views.game_history, name='game_history'), # 전적 페이지
+    path('cancel-game/<int:game_id>/', views.cancel_game, name='cancel_game'), # 게임 취소
+
 ]
--- apps/game/views.py
+++ apps/game/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Game
 
-# Create your views here.
-def main(req):
-    return render(req, 'main.html')
+
+@login_required
+def game_history(request):
+    # 사용자가 공격자인 게임
+    attacks = Game.objects.filter(attacker=request.user).order_by('-id')
+    
+    # 사용자가 수비자인 게임
+    defenses = Game.objects.filter(defender=request.user).order_by('-id')
+    
+    context = {
+        'attacks': attacks,
+        'defenses': defenses,
+    }
+    return render(request, 'list.html', context)
+
+@login_required
+def cancel_game(request, game_id):
+    game = get_object_or_404(Game, id=game_id, attacker=request.user, status='PENDING')
+    game.status = 'CANCELLED'
+    game.save()
+    return redirect('game_history')
