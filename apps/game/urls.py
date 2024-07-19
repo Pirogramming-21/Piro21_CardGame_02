5d30708bb5c6345651247627307a8937b1263c01
shwn <zxz8673@gmail.com>
2024-07-19 10:20:27 +0900

    [add] 기타 수정

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -4,7 +4,6 @@ from .views import *
 app_name = 'game'
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', main, name='main'),
     path('game/attack/', attack, name='attack'), 
     path('game/history/', game_history, name='game_history'), # 전적 (게임 리스트)
@@ -12,12 +11,4 @@ urlpatterns = [
     path('game/<int:pk>/counter/', counter_attack, name='counter_attack'),
     path('game/<int:pk>/cancel/', cancel_game, name='cancel_game'), # 게임 취소
     path('ranking/', ranking, name='ranking'),
-=======
-    path('', views.main, name='main'),
-    path('game-history/', views.game_history, name='game_history'), # 전적 페이지
-    path('cancel-game/<int:game_id>/', views.cancel_game, name='cancel_game'), # 게임 취소
-    path('counter-attack/<int:game_id>/', views.counter_attack, name='counter_attack'), # 반격하기
-    path('game/<int:pk>/', views.game_detail, name='game_detail'),
-    path('game/attack/', views.attack, name='attack'),
->>>>>>> feature/minsu
 ]
